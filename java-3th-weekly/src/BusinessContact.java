f321c87c1060b27ed978cb6e9746022c0301e794
euna kwon <eunakwon@naver.com>
2024-02-08 23:02:45 +0900

    📝 BusinessContact 클래스와 생성자 추가

--- java-3th-weekly/src/BusinessContact.java
+++ java-3th-weekly/src/BusinessContact.java
@@ -1,11 +1,12 @@
-public class BusinessContact{
-    public void displayContacts(){
-
+public class BusinessContact extends Contact{
+    String company;
+    BusinessContact(String name, String phoneNumber, String company){
+        super(name, phoneNumber);
+        this.company = company;
     }
 
-    public void searchContact(name){
-
+    public void print(){
+        System.out.println("이름: " + name + ", 전화번호: " + phoneNumber + ", 회사명: " + company);
     }
 }
-
 
