6e07e6917d824e2af9e6c86c7cb7c4038b43309e
euna kwon <eunakwon@naver.com>
2024-02-08 23:01:59 +0900

    ✨ 모든 인적사항 출력 함수 작성

--- java-3th-weekly/src/AddressBook.java
+++ java-3th-weekly/src/AddressBook.java
@@ -1,7 +1,26 @@
-public class AddressBook extends Contact{
-    String company;
-    AddressBook(String name, String phoneNumber, String company){
-        super(name, phoneNumber);
-        this.company = company;
+import java.util.ArrayList;
+
+public class AddressBook{
+    ArrayList<Contact> contacts;
+
+    public AddressBook(){
+        contacts = new ArrayList<>();
+    }
+    public void addBusiness(String name, String phoneNumber, String company){
+
+    }
+    public void displayContacts(){
+        if (contacts.isEmpty()){
+            System.out.println("연락처를 찾을 수 없습니다.");
+        }
+        else{
+            for (Contact c : contacts){
+                c.print();
+            }
+        }
+    }
+
+    public void searchContact(String name){
+
     }
 }
