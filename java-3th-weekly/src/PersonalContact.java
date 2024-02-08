096ba6943fd13d92fc57e8a9574df6fed9842933
euna kwon <eunakwon@naver.com>
2024-02-08 23:03:20 +0900

    📝 PersonalContact 클래스와 생성자 추가

--- java-3th-weekly/src/PersonalContact.java
+++ java-3th-weekly/src/PersonalContact.java
@@ -4,4 +4,8 @@ public class PersonalContact extends Contact{
         super(name, phoneNumber);
         this.relationship = relationship;
     }
+
+    public void print(){
+        System.out.println("이름: " + name + ", 전화번호: " + phoneNumber + ", 관계: " + relationship);
+    }
 }
