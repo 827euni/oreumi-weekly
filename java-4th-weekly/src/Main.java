3736093b82ffa1b6351e1d5e3d362a5df104d052
euna kwon <eunakwon@naver.com>
2024-02-16 17:37:28 +0900

    ✨ 괄호를 입력할 수 있는 입력함수 작성

--- java-4th-weekly/src/Main.java
+++ java-4th-weekly/src/Main.java
@@ -1,28 +1,33 @@
 import java.util.Stack;
 
 public class Main {
-        boolean solution(String s) {
-            boolean answer = true;
+    static boolean solution(String s) {
+        boolean answer = true;
 
-            Stack<Parentheses> parentheses = new Stack<>();
+        Stack<Parentheses> parentheses = new Stack<>();
 
-            for (char c : s.toCharArray()){
-                if (c=='('){
-                    parentheses.push(new Parentheses(c));
+        for (char c : s.toCharArray()){
+            if (c=='('){
+                parentheses.push(new Parentheses(c));
+            }
+            else if(c==')'){
+                if (parentheses.isEmpty() || parentheses.pop().getSen() != '(') {
+                    return false;
                 }
-                else if(c==')'){
-                    if (parentheses.isEmpty() || parentheses.pop().getSen() != '(') {
-                        return false;
-                    }
-                }
             }
-            if (parentheses.isEmpty()){
-                answer = true;
-            }
-            else{
-                answer = false;
-            }
-            return answer;
         }
+        if (parentheses.isEmpty()){
+            answer = true;
+        }
+        else{
+            answer = false;
+        }
+        return answer;
     }
+    public static void main(String[] args){
+        String s = "((())())";
+        boolean result = solution(s);
+        System.out.println(result);
+    }
+}
 
